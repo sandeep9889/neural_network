11fe440c14beeb320b08483a4919971ea5d22f11
sandeep <sandeepchauhan9630228313@gmail.com>
2021-11-14 10:39:27 +0530

    visualization

---
+++ vis.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot 
+%matplotlib inline
+import numpy as np
+a = np.zeros([3,2])
+a[0,0] = 1
+a[0,1] = 2
+a[1,0] = 9
+a[2,1] = 12
+print(a)
+matplotlib.pyplot.imshow(a,interpolation="nearest")
